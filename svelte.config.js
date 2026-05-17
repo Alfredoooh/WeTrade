65cedbbfc0d440c23ddc96c2183a0c81e3d072c5
Alfredoooh <alfredopjonas@gmail.com>
2026-05-17 17:19:54 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({ out: 'build', fallback: '200.html' })
   }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [sveltekit()]
 });
